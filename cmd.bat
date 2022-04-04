63fc8f3df10ae8c996d980ab8b54972db08a1be5
Dmitriy Kuzmenko <79150928+dsk13-stack@users.noreply.github.com>
2022-04-04 13:23:48 +0300

    Windows cmd

--- cmd.bat
+++ cmd.bat
@@ -4,7 +4,7 @@ ForFiles /p filepath /s /d -n /c "cmd /c del @FILE"
 REM Простой сканер IP адресов подсети с записью результатов в файл
 chcp 65001
 echo off
-for /L %i in (1, 1, 255) do if %i EQU 255(echo Scan finished) else(ping /n 1 /w 100 192.168.1.%i) | findstr "TTL" >> C:\ScanResult.txt
+for /L %i in (1, 1, 255) do if %i EQU 255 (echo Scan finished) else (ping /n 1 /w 100 192.168.1.%i) | findstr "TTL" >> C:\ScanResult.txt
 pause
 
 REM Копия файлов, по изменению 
